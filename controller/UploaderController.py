26d07ca23d8ccfaacf7b00741dd520e6e1df6299
Bayron Vazquez <bayron.vazquez@sandox.com.mx>
2024-01-20 19:07:15 -0600

    Agregando controlador para ubir los datos al server

---
+++ controller/UploaderController.py
@@ -0,0 +1,8 @@
+# UploaderController
+#
+# Esta clase se encarga de subir los archivos recolectados al servidor.
+
+class UploaderController:
+    def __init__(self):
+        self.dominio = "https://soft-tech-mx.website/"
+        
